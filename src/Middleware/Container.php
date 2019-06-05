ec1270c51ac4ca82dc89a344a7030c9eb7e8d025
Andreas Sundqvist <andreas@forme.se>
2019-06-05 11:36:07 +0200

    Don't use a queue to store middlewares

--- src/Middleware/Container.php
+++ src/Middleware/Container.php
@@ -3,9 +3,12 @@
 namespace Circli\WebCore\Middleware;
 
 use Psr\Http\Server\MiddlewareInterface;
+use Traversable;
 
-class Container extends \SplPriorityQueue
+final class Container implements \IteratorAggregate
 {
+    private $data = [];
+
     public const DEFAULT_PRIORITY = 500;
     private const MAX_PRE_PRIORITY = 1000;
     private const MIN_POST_PRIORITY = 1000;
@@ -19,15 +22,24 @@ class Container extends \SplPriorityQueue
         }
     }
 
+    public function insert($middleware, int $priority): void
+    {
+        if (!isset($this->data[$priority])) {
+            $this->data[$priority] = [];
+        }
+        $this->data[$priority][] = $middleware;
+    }
+
     /**
      * @param string|MiddlewareInterface $middleware
      * @param int $priority
      */
     public function addPreRouter($middleware, int $priority = self::DEFAULT_PRIORITY): void
     {
-        if ($priority < self::MAX_PRE_PRIORITY) {
-            $priority = self::MAX_PRE_PRIORITY + 1;
+        if ($priority > self::MAX_PRE_PRIORITY) {
+            $priority = self::MAX_PRE_PRIORITY - 1;
         }
+
         $this->insert($middleware, $priority);
     }
 
@@ -37,9 +49,22 @@ class Container extends \SplPriorityQueue
      */
     public function addPostRouter($middleware, int $priority = 2000): void
     {
-        if ($priority > self::MIN_POST_PRIORITY) {
-            $priority = self::MIN_POST_PRIORITY - 1;
+        if ($priority < self::MIN_POST_PRIORITY) {
+            $priority = self::MIN_POST_PRIORITY + 1;
         }
+
         $this->insert($middleware, $priority);
+    }
+
+    public function getIterator()
+    {
+        $data = $this->data;
+        ksort($data);
+        $tmp = [];
+        foreach ($data as $middlewares) {
+            $tmp[] = $middlewares;
+        }
+
+        return new \ArrayIterator(array_merge(...$tmp));
     }
 }

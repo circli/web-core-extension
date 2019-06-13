e1ec53ac33f3a0f4960ecfa9231b80b6aec42b51
Andreas Sundqvist <andreas@forme.se>
2019-06-13 21:50:36 +0200

    Fix problem with not found handler
    Now it can handle 404s that happens after this middleware

--- src/Middleware/NotFoundHandler.php
+++ src/Middleware/NotFoundHandler.php
@@ -23,50 +23,61 @@ class NotFoundHandler implements MiddlewareInterface
 
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
+        $reDispatch = clone $handler;
         $route = $request->getAttribute('route');
         if ($route instanceof RouteInterface && $route->getStatus() === RouterDispatcherInterface::NOT_FOUND) {
-            $newRoute = new class($this->notFoundAction, $route) implements RouteInterface {
-                /** @var ActionInterface */
-                private $action;
-                /** @var RouteInterface */
-                private $route;
-
-                public function __construct(ActionInterface $action, RouteInterface $route)
-                {
-                    $this->action = $action;
-                    $this->route = $route;
-                }
-
-                public function getStatus(): int
-                {
-                    return RouterDispatcherInterface::FOUND;
-                }
-
-                public function getAllows(): array
-                {
-                    return $this->route->getAllows();
-                }
-
-                public function getHandler()
-                {
-                    return $this->action;
-                }
-
-                public function getMethod()
-                {
-                    return $this->route->getMethod();
-                }
-
-                public function getAttributes(): array
-                {
-                    return $this->route->getAttributes();
-                }
-            };
-
-            $response = $handler->handle($request->withAttribute('route', $newRoute));
-            return $response->withStatus(404);
+            return $this->dispatchNotFound($request, $handler, $route);
         }
 
-        return $handler->handle($request);
+        $response = $handler->handle($request);
+        if ($response->getStatusCode() === 404) {
+            return $this->dispatchNotFound($request, $reDispatch, $route);
+        }
+        return $response;
+    }
+
+
+    private function dispatchNotFound(ServerRequestInterface $request, RequestHandlerInterface $handler, RouteInterface $route)
+    {
+        $newRoute = new class($this->notFoundAction, $route) implements RouteInterface {
+            /** @var ActionInterface */
+            private $action;
+            /** @var RouteInterface */
+            private $route;
+
+            public function __construct(ActionInterface $action, RouteInterface $route)
+            {
+                $this->action = $action;
+                $this->route = $route;
+            }
+
+            public function getStatus(): int
+            {
+                return RouterDispatcherInterface::FOUND;
+            }
+
+            public function getAllows(): array
+            {
+                return $this->route->getAllows();
+            }
+
+            public function getHandler()
+            {
+                return $this->action;
+            }
+
+            public function getMethod()
+            {
+                return $this->route->getMethod();
+            }
+
+            public function getAttributes(): array
+            {
+                return $this->route->getAttributes();
+            }
+        };
+
+        $response = $handler->handle($request->withAttribute('route', $newRoute));
+        return $response->withStatus(404);
     }
 }

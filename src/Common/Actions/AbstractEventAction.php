e85762f0c0c9643f250a7fd33d91568a0d9ca667
Andreas Sundqvist <andreas@stefna.is>
2021-11-08 16:18:53 +0100

    Move abstract action into common package

--- src/Common/Actions/AbstractEventAction.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace Circli\WebCore\Common\Actions;
-
-use Polus\Adr\Interfaces\EventAction;
-
-abstract class AbstractEventAction extends AbstractAction implements EventAction
-{
-}
